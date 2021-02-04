d7b78d95fdde440e57178c020275b52ec014c499
Isaxc0 <78422713+Isaxc0@users.noreply.github.com>
2021-02-04 16:46:23 +0000

    Update distributional_semantics.py

--- src/distributional_semantics.py
+++ src/distributional_semantics.py
@@ -1,11 +1,4 @@
-#preliminary imports
 import re
-import random
-import math
-import pandas as pd
-import matplotlib.pyplot as plt
-from itertools import zip_longest
-
 import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -16,8 +9,6 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import wordnet as wn
 from nltk.corpus import wordnet_ic as wn_ic
-brown_ic = wn_ic.ic("ic-brown.dat")
-
 
 from sussex_nltk.corpus_readers import ReutersCorpusReader
 
@@ -53,31 +44,6 @@ def generate_features(sentences,window=1):
                 current[feature]=current.get(feature,0)+1
             mydict[token]=current
     return mydict
-
-def find_frequently_occurring_words(sentences):
-  """
-  Finds the most frequent words in the sample. Then using those top words, finds the top 1000 which have at least one noun sense. Removes stop words and punctuation
-
-  :param sentences: A list where each element is a list of tokens
-  :return: A list of 1000 most frequent words which have at least one noun sense according to WordNet.
-  """
-
-  frequent_words = {} # key:word   value:number of times it appears in sample
-  for sentence in sentences: #iterates over each sentence
-    for word in sentence: #iterates over each word in sentence
-      if word not in frequent_words.keys(): #word not discovered before
-        frequent_words[word] = 1  #added to the dictionary for the first time with a frequency of 1
-      else: #word previously discovered
-        frequent_words[word] = frequent_words.get(word) + 1 #its frequency incremented by 1
-
-  sort_freq_words = dict(sorted(frequent_words.items(), key=lambda item: item[1],reverse=True)) #sorts dictionary of word frequency from largest to smallest frequencey
-  final_sorted = []
-  for word in sort_freq_words.keys(): #iterates over words in the top words starting from largest
-    count = len((wn.synsets(word,wn.NOUN))) #gets the number of noun senses
-    if count > 0: #needs to be added final top 1000
-      final_sorted.append(word)
-    if len(final_sorted) >= 1000: #returns when top 1000 words are found
-      return final_sorted
 
 
 def semantic_similarity(word1, word2, speech, measure):
